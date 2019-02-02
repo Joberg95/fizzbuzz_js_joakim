aedd33d83a04db7e9b6d8aa35387d2d874bc0038
nevroje <joakim.berglind@gmail.com>
2019-02-02 17:23:15 +0100

    set up feature tests, Working on solving assertion error

--- features/application.feature.js
+++ features/application.feature.js
@@ -1,26 +1,24 @@
 require('../spec.helper');
 
 context('User can input a value and get FizzBuzz results', () => {
-  // Initialize a browser and visit the server's root path
   before(async () => {
     await browser.init()
     await browser.visitPage('http://localhost:8080/')
   });
 
-  // Reload before each test 
   beforeEach(async () => {
     await browser.page.reload();
   });
 
-  // Make sure the browser closes after the test is finished
   after(() => {
     browser.close();
   });
 
-  it ('clicking on the "Check" button', async () => {
+  it ('clicking on the "check" button', async () => {
     await browser.fillIn("input[id='value']", {with:  "3"})
     await browser.clickOnButton("input[value='Check']")
     let content = await browser.getContent("[id='display_answer']")
     expect(content).to.eql('Fizz')
   })
+
 });
